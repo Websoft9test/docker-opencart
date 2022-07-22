05a68876117c6575cead27d3997f5266b62ad29b
qiaofeng1227 <76487013@qq.com>
2022-07-22 11:42:24 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "opencart version:" $(docker exec -i $1 cat /bitnami/opencart/upload/admin/index.php  |grep VERSION |cut -d"'" -f4) |sudo tee -a /data/logs/install_version.txt
+sudo echo "opencart version:" $(docker exec -i $1 cat /opt/bitnami/opencart/index.php  |grep VERSION |cut -d"'" -f4) |sudo tee -a /data/logs/install_version.txt
