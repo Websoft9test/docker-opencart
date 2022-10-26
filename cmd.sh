3c47cbdc793ee9e40831660244bd7a1af32339b6
qiaofeng1227 <76487013@qq.com>
2022-10-26 14:33:14 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -2,7 +2,7 @@
 # customrized cmd powered by Websoft9
 
 if [ ! -f "/var/www/html/install/controller/install/step_3.php" ];then
-  echo "初始化文件不存在，不再执行初始化动作"
+  echo "初始化文件不存在，不再执行初始化动作!"
   else
   echo "初始化文件存在，执行初始化动作"
   sed -i "218s/'localhost'/'$MYSQL_HOST'/" /var/www/html/install/controller/install/step_3.php
